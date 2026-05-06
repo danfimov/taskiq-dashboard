2bb0a0ddc34c99cf996a909e067b6f02fe9b2bec
Anfimov Dima <dmitrii.anfimov@truv.com>
2026-05-06 10:11:56 +0200

    chore: do not trigger css minification during local package builds

--- hatch_build.py
+++ hatch_build.py
@@ -1,3 +1,4 @@
+import os
 import subprocess
 
 from hatchling.builders.hooks.plugin.interface import BuildHookInterface
@@ -5,5 +6,7 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class CustomBuildHook(BuildHookInterface):
     def initialize(self, _version: str, _build_data: dict) -> None:
+        if not bool(os.environ.get('GITHUB_ACTIONS', '')):  # do not minify css styles locally
+            return
         subprocess.run(['bun', 'install', '--frozen-lockfile'], check=True)  # noqa: S607
         subprocess.run(['bun', 'run', 'build'], check=True)  # noqa: S607
